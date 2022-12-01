456598d57b03027c8c0e386cd24feb6a7df24835
jkirikal <johan@vhk.ee>
2022-12-01 15:21:15 +0200

    new icon for page, minor tweaks in html files and js

--- src/feedback.js
+++ src/feedback.js
@@ -1,7 +1,13 @@
 function mouseOver() {
-    document.getElementById("hover").style.color = "#b5daa6";
+    document.getElementById("email").style.color = "#b5daa6";
 }
-  
+//näidatud javascriptiga, kuidas elemendi värvi muuta
 function mouseOut() {
-    document.getElementById("hover").style.color = "#0f4940";
+    document.getElementById("email").style.color = "#0f4940";
 }
+
+function sendEmail() 
+{
+    window.location = "mailto:tudengiretseptid@gmail.com";
+}
+//meili saatmise funktsiooniks kasutatud allikat: https://stackoverflow.com/questions/19639900/use-div-as-a-button-and-trigger-a-mailto-when-it-is-clicked
--- src/homepage.js
+++ src/homepage.js
@@ -1,80 +1,83 @@
-window.onload = function() {
+// kood on Johan Kirikali poolt kirjutatud ning selle eesmärk on luua retseptielemente vastavalt sellele, mis recipes.json failis muutub.
+// See on selleks vajalik, et kui tulevikus tahta lihtsamalt retsepte juurde lisada/muuta, siis saab seda teha kõike mugavalt ja loetavalt,
+// ning vajadusel saab retseptielementidesse veel automaatikat lisada.
 
 
-    let elements
-        fetch('data/recipes.json')
-            .then((response) => response.json())
-            .then(json => {
-                let main_div = document.getElementById("main")
-                for(i=0;i<json.length;i++){
-                    let item = json[i];
-                    let divfood = document.createElement('div');
+fetch('data/recipes.json')
+    .then((response) => response.json())
+    .then(json => {
+        let main_div = document.getElementById("main")
+        //loob iga retsepti kohta html sektsiooni
+        for(i=0;i<json.length;i++){
+            let item = json[i];
+            let divfood = document.createElement('div');
 
-                    let divbasic = document.createElement('div');
+            let divbasic = document.createElement('div');
 
+            //pealkiri
+            let namespan = document.createElement('span');
+            let nameh3 = document.createElement('h3');
+            nameh3.innerText = item.name;
+            namespan.appendChild(nameh3);
 
-                    let namespan = document.createElement('span');
-                    let nameh3 = document.createElement('h3');
-                    nameh3.innerText = item.name;
-                    namespan.appendChild(nameh3);
+            //retsepti kirjeldus
+            let basicspan = document.createElement('span');
+            let basich4 = document.createElement('h4');
+            let basiclist = document.createElement('ul');
 
-                    let basicspan = document.createElement('span');
-                    let basich4 = document.createElement('h4');
-                    let basiclist = document.createElement('ul');
-                    for(var a in item.basicData){
-                        let singlebasic = a + ": " + item.basicData[a].toString();
-                        let basicdata = document.createElement('li');
-                        basicdata.innerText = singlebasic;
-                        basiclist.appendChild(basicdata);   
-                    }
-                    basich4.appendChild(basiclist);
-                    basicspan.appendChild(basich4);
-                    basicspan.className = "food_basic";
+            //koostab retsepti kirjeldustest unordered listi
+            for(var a in item.basicData){
+                let singlebasic = a + ": " + item.basicData[a].toString();
+                let basicdata = document.createElement('li');
+                basicdata.innerText = singlebasic;
+                basiclist.appendChild(basicdata);   
+            }
 
-                    divbasic.appendChild(namespan);
-                    divbasic.appendChild(basicspan);
-                    divbasic.className="basic_inf"
+            
+            basich4.appendChild(basiclist);
+            basicspan.appendChild(basich4);
+            basicspan.className = "food_basic";
 
-                    let divinstruct = document.createElement('div');
-                    let divitems = document.createElement('div');
-                    let itemslist = document.createElement('ul');
-                    for(a=0;a<item.items.length;a++){
-                        let singleitem = item.items[a];
-                        let singledata = document.createElement('li');
-                        singledata.innerText = singleitem;
-                        itemslist.appendChild(singledata)
-                    }
-                    divitems.appendChild(itemslist);
-                    divitems.className = "foodIngredients"
+            divbasic.appendChild(namespan);
+            divbasic.appendChild(basicspan);
+            divbasic.className="basic_inf"
 
+            let divinstruct = document.createElement('div');
+            let divitems = document.createElement('div');
+            let itemslist = document.createElement('ul');
 
-                    let textdiv = document.createElement('div');
-                    textdiv.className = "itemInstructions";
-                    let instructp = document.createElement('p');
-                    instructp.innerText = item.instruction;
-                    textdiv.appendChild(instructp);
-                    
-                    divinstruct.appendChild(divitems);
-                    divinstruct.appendChild(textdiv);
-                    divinstruct.className="instructions"
+            //koostab vajalikest toiduainetest unordered listi
+            for(a=0;a<item.items.length;a++){
+                let singleitem = item.items[a];
+                let singledata = document.createElement('li');
+                singledata.innerText = singleitem;
+                itemslist.appendChild(singledata)
+            }
+            divitems.appendChild(itemslist);
+            divitems.className = "foodIngredients"
 
-                    divfood.appendChild(divbasic);
-                    divfood.appendChild(divinstruct);
-                    divfood.className="food";
-                   
-                    main_div.appendChild(divfood);
-                    
-                }
+            //toidu valmistamise õpetus
+            let textdiv = document.createElement('div');
+            textdiv.className = "itemInstructions";
+            let instructp = document.createElement('p');
+            instructp.innerText = item.instruction;
+            textdiv.appendChild(instructp);
+            
+            divinstruct.appendChild(divitems);
+            divinstruct.appendChild(textdiv);
+            divinstruct.className="instructions"
+
+            divfood.appendChild(divbasic);
+            divfood.appendChild(divinstruct);
+            divfood.className="food";
+            
+            main_div.appendChild(divfood);
+            
+        }
+
+    })
+    .catch(err => {
+        //logib errorid konsooli
+        console.log(err)
+    })
     
-            })
-            .catch(err => {
-                let errDiv = document.createElement('div');
-                errDiv.className = 'post';
-                errDiv.innerText = err;
-                document.body.appendChild(errDiv);
-            })
-        
-        
-    
-    
-    }
